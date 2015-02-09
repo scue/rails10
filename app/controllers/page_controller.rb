291bb018ff28d8f0cb663e120f103ae314e935ef
scue <linkscue@gmail.com>
2015-02-10 02:08:30 +0800

    发布消息界面：使用Partial，与本地的 实例变量 结合，准备向数据库进军
        太深夜了，改天再写吧。。( ´∀`)σ)Д`)

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -1,5 +1,6 @@
 class PageController < ApplicationController
     def welcome
-        
+        # 这里一共定义了两个issues
+        @issues  = [ { title: "Ruby lovers", comments: "4" }, { title: "Rails and Laravel", comments: "5" } ]
     end
 end
