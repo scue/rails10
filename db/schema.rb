ba2ac29afd92a1796d2e645cf00f5bab6119d717
scue <linkscue@gmail.com>
2015-02-11 00:11:25 +0800

    使用数据库： 初次使用数据库
        rails g migration CreateIssues
        /* edit db/migrate/20150210155939_create_issues.rb */
        rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20150210155939) do
+
+  create_table "issues", force: :cascade do |t|
+    t.string   "title"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
