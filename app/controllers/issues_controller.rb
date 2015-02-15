1dd73a15839750eef54995c4a6561476f266ee95
scue <linkscue@gmail.com>
2015-02-16 01:38:49 +0800

    (11-1)瞄准一个东东，来 CRUD: 删除评论

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -2,4 +2,9 @@ class IssuesController < ApplicationController
   def show
      @issue = Issue.find(params[:id])
   end
+  def destroy
+    issue = Issue.find(params[:id])
+    issue.destroy
+    redirect_to :root
+  end
 end
